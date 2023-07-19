7249fdda3925f806aafe254071576affbdff0522
lalit <lalitbisht540@gmail.com>
2023-07-20 00:14:48 +0530

    https://github.com/LB540/Cricket_Chrome_Extension.git

---
+++ script.js
@@ -0,0 +1,18 @@
+async function getMatchData() {
+    return await fetch("https://api.cricapi.com/v1/currentMatches?apikey=5e9f82cb-2b32-45fb-8ba1-26c48fc1751a&offset=0")
+        .then(data => data.json())
+        .then(data =>{
+            if(data.status !="success") return;
+            const matchesList = data.data;
+
+            if(!matchesList) return [];
+
+            const relevantData = matchesList.map(match => `${match.name}, ${match.status}`);
+            console.log({relevantData});
+            document.getElementById("matches").innerHTML = relevantData.map(match => `<li>${match}</li>`).join('');
+            return relevantData;
+        })
+        .catch(e =>console.log(e));
+}
+
+getMatchData();
